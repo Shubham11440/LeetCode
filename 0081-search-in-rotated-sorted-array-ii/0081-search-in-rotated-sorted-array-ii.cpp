8f80632f4d03c61ae3e3431844e2930cff40e58d
Shubham Mali <shubhamvmali09@gmail.com>
2025-02-07 20:17:40 +0530

    Time: 0 ms (100%), Space: 18 MB (54.83%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -4,32 +4,27 @@ public:
         int n = nums.size();
         int low = 0, high = n - 1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            
-            if (nums[mid] == target) return true;
+        while(low <= high){
+            int mid = (low + high)/2;
 
-            // Handling duplicates
-            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
-                low++, high--;
+            if(nums[mid] == target) return true;
+
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++,high--;
                 continue;
             }
 
-            // Check if the left half is sorted
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target < nums[mid]){
                     high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
+                } 
+                else low = mid + 1;
             }
-            // Right half is sorted
-            else {
-                if (nums[mid] < target && target <= nums[high]) {
+            else{
+                if(nums[mid] < target && target <= nums[high]){
                     low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+                } 
+                else high = mid - 1;
             }
         }
         return false;
