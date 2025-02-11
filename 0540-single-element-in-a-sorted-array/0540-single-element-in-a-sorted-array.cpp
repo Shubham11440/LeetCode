243307c1ebd5c5da941ec25e4ee523d25c439431
Shubham Mali <shubhamvmali09@gmail.com>
2025-02-11 23:04:12 +0530

    Time: 0 ms (100%), Space: 26.2 MB (34.28%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
          int n = nums.size();
 
-        if (n == 1) return nums[0];  // Fix out-of-bounds error
+        if (n == 1) return nums[0];  
         if (nums[0] != nums[1]) return nums[0];
         if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
 
