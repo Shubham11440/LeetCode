40c6bd2805a73a41268a6a3045528c2775f31a33
Shubham Mali <shubhamvmali09@gmail.com>
2025-02-14 19:39:02 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.08%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int ans = sqrt(x);
-        return ans;
+        long long low = 1, high = x;
+
+        while(low <= high){
+            long long mid = (low + high)/2;
+            long long val = mid * mid;
+
+            if(val <= (long long) x) {
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return high;
     }
 };
