f65a8d7e8f4c08e97895aabcd2b92103bc4f679e
Shubham Mali <shubhamvmali09@gmail.com>
2025-06-10 11:42:11 +0530

    Time: 8 ms (81.24%), Space: 11.7 MB (96.5%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,16 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* temp = head;
-        unordered_map<ListNode*,int> nodeMap;
+    ListNode* slow = head;
+    ListNode* fast = head;
 
-        while(temp != NULL){
-            if(nodeMap.find(temp) != nodeMap.end()){
-                return true;
-            }
-            nodeMap[temp] = 1;
-            temp = temp -> next;
-        }
-        return false;
+    while(fast != NULL && fast -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+
+        if(slow == fast) return true;
+    }
+    return false;
     }
 };
